896d87be6ff2571b763c0c05c249e0e868ebb384
Sachin Jadhao <130213251+SachinJadhao71@users.noreply.github.com>
2023-04-10 08:56:16 +0530

    Add files via upload

---
+++ SnakeLadder/src/main/java/com/example/snakeladder/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.example.snakeladder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+   private Pane createContent(){
+       Pane root = new Pane();
+       return root;
+   }
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(createContent());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
