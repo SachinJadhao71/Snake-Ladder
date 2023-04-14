ddff616db52ba12585e5436664523295ae92bfd4
Sachin Jadhao <130213251+SachinJadhao71@users.noreply.github.com>
2023-04-14 14:54:41 +0530

    Add files via upload

--- SnakeLadder/src/main/java/com/example/snakeladder/Player.java
+++ SnakeLadder/src/main/java/com/example/snakeladder/Player.java
@@ -1,7 +1,11 @@
 package com.example.snakeladder;
 
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class Player {
    private Circle coins;
@@ -13,19 +17,61 @@ public class Player {
     Player(int tilesize , Color coinColor, String palyerName){
         coins = new Circle(tilesize/2);
         coins.setFill(coinColor);
-        currentPosition = 1;
+        currentPosition = 0;
+        movePlayer(1);
         name = palyerName;
     }
     public void movePlayer(int diceValue){
-        if(currentPosition + diceValue <= 100)
-        currentPosition+=diceValue;
+        if(currentPosition + diceValue <= 100){
+            currentPosition+=diceValue;
 
-        int x= gameBoard.getXCordinate(currentPosition);
-        int y = gameBoard.getYCordinate(currentPosition);
+            TranslateTransition secondMove = null, firstMove = translateAnimation(diceValue);
 
 
-        coins.setTranslateX(x);
-        coins.setTranslateY(y);
+
+            int newPosition = gameBoard.getNewPosition(currentPosition);
+
+            if(newPosition != currentPosition && newPosition != -1){
+                currentPosition = newPosition;
+                secondMove = translateAnimation(6);
+            }
+            if(secondMove == null){
+                firstMove.play();
+            }
+            else{
+                SequentialTransition sequentialTransition = new SequentialTransition(firstMove,
+                        new PauseTransition(Duration.millis(1000)), secondMove );
+
+                sequentialTransition.play();
+            }
+        }
+
+
+//        int x= gameBoard.getXCordinate(currentPosition);
+//        int y = gameBoard.getYCordinate(currentPosition);
+//        coins.setTranslateX(x);
+//        coins.setTranslateY(y);
+
+
+    }
+    private TranslateTransition translateAnimation(int diceValue){
+        TranslateTransition animate = new TranslateTransition(Duration.millis(200*diceValue),coins);
+        animate.setToX(gameBoard.getXCordinate(currentPosition));
+        animate.setToY(gameBoard.getYCordinate(currentPosition));
+        animate.setAutoReverse(false);
+        return animate;
+    }
+
+    public void startingPosition(){
+        currentPosition=0;
+        movePlayer(1);
+    }
+
+    boolean isWinner(){
+        if(currentPosition==100){
+            return true;
+        }
+        return false;
     }
 
     public Circle getCoins() {
--- SnakeLadder/src/main/java/com/example/snakeladder/SnakeAndLadder.java
+++ SnakeLadder/src/main/java/com/example/snakeladder/SnakeAndLadder.java
@@ -1,12 +1,15 @@
 package com.example.snakeladder;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +17,10 @@ import java.io.IOException;
 public class SnakeAndLadder extends Application {
     public static final int tilesize=40, width=10, height=10;
     public static final int buttonLine = tilesize*height + 50, infoLine = buttonLine-30;
+    private static Dice dice = new Dice();
+    private Player playerOne,playerTwo;
+
+    private boolean gameStarted = false, playerOneTurn = false, playerTwoTurn = false;
 
    private Pane createContent(){
        Pane root = new Pane();
@@ -40,27 +47,140 @@ public class SnakeAndLadder extends Application {
 
        playerOneButton.setTranslateY(buttonLine);
        playerOneButton.setTranslateX(20);
+       playerOneButton.setDisable(true);
        playerTwoButton.setTranslateY(buttonLine);
        playerTwoButton.setTranslateX(300);
+       playerTwoButton.setDisable(true);
        startButton.setTranslateY(buttonLine);
        startButton.setTranslateX(180);
 
-       Label playerOneLable = new Label("Your Turn !");
-       Label palyerTwoLabel = new Label("Your Turn !");
-       Label startButtonLable = new Label("Start");
+       Label playerOneLable = new Label("");
+       Label palyerTwoLabel = new Label("");
+       Label diceLabel = new Label("Start");
 
 
        playerOneLable.setTranslateY(infoLine);
        playerOneLable.setTranslateX(20);
        palyerTwoLabel.setTranslateY(infoLine);
        palyerTwoLabel.setTranslateX(300);
-       startButtonLable.setTranslateY(infoLine);
-       startButtonLable.setTranslateX(180);
+       diceLabel.setTranslateY(infoLine);
+       diceLabel.setTranslateX(180);
+
+       playerOne = new Player(tilesize, Color.BLACK , "Amit");
+       playerTwo = new Player(tilesize-5, Color.WHITE, "Sachin");
+
+
+       playerOneButton.setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent actionEvent) {
+               if(gameStarted) {
+                   if (playerOneTurn) {
+                       int diceValue = dice.getRolledDicevalue();
+
+                       diceLabel.setText("Dice Value : " + diceValue);
+                       playerOne.movePlayer(diceValue);
+//                       winning condition
+                       if(playerOne.isWinner()){
+                           diceLabel.setText("The Winner Is : " + playerOne.getName());
+                           playerOneTurn = false;
+                           playerOneButton.setDisable(true);
+                           playerOneLable.setText("");
+
+                           playerTwoTurn = true;
+                           playerTwoButton.setDisable(true);
+                           palyerTwoLabel.setText("");
+
+                           startButton.setDisable(false);
+                           startButton.setText("Restart Game");
+
+                           gameStarted = false;
+                       }
+                       else{
+                           playerOneTurn = false;
+                           playerOneButton.setDisable(true);
+                           playerOneLable.setText("");
+
+                           playerTwoTurn = true;
+                           playerTwoButton.setDisable(false);
+                           palyerTwoLabel.setText("Your Turn " + playerTwo.getName());
+                       }
 
 
 
 
-       root.getChildren().addAll(board,playerOneButton,playerTwoButton,startButton,playerOneLable,palyerTwoLabel,startButtonLable);
+                   }
+               }
+           }
+       });
+
+       playerTwoButton.setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent actionEvent) {
+               if(gameStarted) {
+                   if (playerTwoTurn) {
+                       int diceValue = dice.getRolledDicevalue();
+
+                       diceLabel.setText("Dice Value : " + diceValue);
+                       playerTwo.movePlayer(diceValue);
+
+//                       winning condition
+                       if(playerTwo.isWinner()){
+                           diceLabel.setText("The Winner Is : " + playerTwo.getName());
+                           playerOneTurn = false;
+                           playerOneButton.setDisable(true);
+                           playerOneLable.setText("");
+
+                           playerTwoTurn = true;
+                           playerTwoButton.setDisable(true);
+                           palyerTwoLabel.setText("");
+
+                           startButton.setDisable(false);
+                           startButton.setText("Restart Game");
+                       }
+                       else{
+                           playerOneTurn = true;
+                           playerOneButton.setDisable(false);
+                           playerOneLable.setText("Your Turn " + playerOne.getName());
+
+                           playerTwoTurn = false;
+                           playerTwoButton.setDisable(true);
+                           palyerTwoLabel.setText("");
+                       }
+
+
+
+                   }
+               }
+           }
+       });
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                gameStarted =true;
+                diceLabel.setText("Game Started");
+
+                startButton.setDisable(true);
+
+                playerOneTurn = true;
+
+                playerOneLable.setText("Your Turn " + playerOne.getName());
+
+                playerOneButton.setDisable(false);
+                playerOne.startingPosition();
+
+                playerTwoTurn = false;
+                palyerTwoLabel.setText("");
+
+                playerTwoButton.setDisable(true);
+                playerTwo.startingPosition();
+            }
+        });
+
+
+       root.getChildren().addAll(board,playerOneButton,playerTwoButton,startButton,
+               playerOneLable,palyerTwoLabel,diceLabel, playerOne.getCoins(), playerTwo.getCoins()
+       );
        return root;
    }
     @Override
